8388c5baddc5c0452ea2d2ab617dd8a9b96eebe5
Rezu0 <yunianrezky031@gmail.com>
2022-01-11 21:50:04 +0700

    menambahkan form input

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('app.layout')
+@section('title', 'NoteApps')
+
+@section('content')
+    
+<div class="row">
+    <h2 class="my-3 fw-bolder ps-5">Simple Todo List or Note Apps</h2>
+
+    <form action="/todo" method="post">
+        
+    </form>
+</div>
+
+@endsection
