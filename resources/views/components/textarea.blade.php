471ba68f3f4c86c5fd7ba527fcb14423d2012464
Rezu0 <yunianrezky031@gmail.com>
2022-01-11 22:31:50 +0700

    textarea.blade.php

---
+++ resources/views/components/textarea.blade.php
@@ -0,0 +1,4 @@
+<label for="{{ $label }}">{{ ucfirst($label) }}</label>
+<div class="col-12 my-3">
+    <textarea name="{{ $field }}" id="{{ $field }}" class="textareaInput" placeholder="{{ $placeholder }}"></textarea>
+</div>
